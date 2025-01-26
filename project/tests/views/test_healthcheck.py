fbbdeb55a832e62e668d099ba7c132aef69093c9
Cristian Castillo <cristian.castillo@thepowermba.com>
2025-01-26 18:43:48 +0100

    feat: setup server and docker build action

---
+++ project/tests/views/test_healthcheck.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HealthcheckTests(TestCase):
+    def test_healthcheck(self):
+        response = self.client.get('/api/healthcheck/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'status': 'ok'})
